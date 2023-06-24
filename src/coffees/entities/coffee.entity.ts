60826d7335b3b90daeeeb372fb120b7745723ea9
Luis Fernandez <lfergon@gmail.com>
2023-06-24 17:46:43 +0200

    feat: database integration sql

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,30 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity() // sql table === 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
-  flavors: string[];
+
+  @JoinTable() // 👈 Join the 2 tables - only the OWNER-side does this
+  @ManyToMany(
+    type => Flavor,
+    (flavor) => flavor.coffees, // what is "coffee" within the Flavor Entity
+    {
+      cascade: true,
+    },
+  ) // 👈
+  flavors: Flavor[];
 }
