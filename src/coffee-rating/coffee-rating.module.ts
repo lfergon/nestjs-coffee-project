9807f8ad9b4444f9d2e048ffce5ef1d26f223a58
Luis Fernandez <lfergon@gmail.com>
2023-07-02 11:21:42 +0200

    feat: filter, guard

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'postgres',
+      username: 'postgres',
+    }),
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,9 +5,10 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
