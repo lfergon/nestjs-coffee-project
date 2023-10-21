ed6d1d048522e1c5e931a9ae6111955569b174a1
Luis Fernandez <lfergon@gmail.com>
2023-10-21 20:38:28 +0200

    test: added tests to controllers and guard

--- src/coffees/coffees.controller.spec.ts
+++ src/coffees/coffees.controller.spec.ts
@@ -1,18 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesController } from './coffees.controller';
+import { DataSource } from 'typeorm';
+import { CoffeesService } from './coffees.service';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const dataSourceMockFactory: () => MockType<DataSource> = jest.fn(() => ({
+  getCoffee: jest.fn(),
+}));
+
+type MockType<T> = {
+  [P in keyof T]?: jest.Mock;
+};
 
 describe('CoffeesController', () => {
-  let controller: CoffeesController;
+  let coffeesController: CoffeesController;
+  let coffeesService: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CoffeesController],
+      providers: [
+        CoffeesService,
+        {
+          provide: getRepositoryToken(Coffee),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Flavor),
+          useValue: {},
+        },
+        {
+          provide: DataSource,
+          useFactory: dataSourceMockFactory,
+        },
+      ],
     }).compile();
 
-    controller = module.get<CoffeesController>(CoffeesController);
+    coffeesController = module.get<CoffeesController>(CoffeesController);
+    coffeesService = module.get<CoffeesService>(CoffeesService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(coffeesController).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of coffees', async () => {
+      const paginationQuery: PaginationQueryDto = { limit: 0, offset: 0 };
+      const coffees: any[] = [];
+
+      jest.spyOn(coffeesService, 'findAll').mockResolvedValue(coffees);
+
+      expect(await coffeesController.findAll(paginationQuery)).toBe(coffees);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a coffee by ID', async () => {
+      const coffeeId = 1;
+      const coffee: any = {};
+
+      jest.spyOn(coffeesService, 'findOne').mockResolvedValue(coffee);
+
+      expect(await coffeesController.findOne(coffeeId)).toBe(coffee);
+    });
   });
 });
--- src/common/guards/api-key.guard.spec.ts
+++ src/common/guards/api-key.guard.spec.ts
@@ -1,7 +1,88 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Reflector } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 import { ApiKeyGuard } from './api-key.guard';
 
 describe('ApiKeyGuard', () => {
+  let apiKeyGuard: ApiKeyGuard;
+  let reflector: Reflector;
+  let configService: ConfigService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ApiKeyGuard,
+        Reflector,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    apiKeyGuard = module.get<ApiKeyGuard>(ApiKeyGuard);
+    reflector = module.get<Reflector>(Reflector);
+    configService = module.get<ConfigService>(ConfigService);
+  });
+
   it('should be defined', () => {
-    expect(new ApiKeyGuard()).toBeDefined();
+    expect(apiKeyGuard).toBeDefined();
+  });
+
+  it('should return true if the route is marked as public', () => {
+    jest.spyOn(reflector, 'get').mockReturnValue(true);
+
+    const context = {
+      getHandler: jest.fn(),
+      switchToHttp: () => ({
+        getRequest: jest.fn(),
+      }),
+    } as unknown as ExecutionContext;
+
+    expect(apiKeyGuard.canActivate(context)).toBe(true);
+  });
+
+  it('should return true if the API key in the request header matches the config', () => {
+    jest.spyOn(reflector, 'get').mockReturnValue(false);
+    jest.spyOn(configService, 'get').mockReturnValue('your-api-key');
+
+    const request = {
+      header: (name: string) => (name === 'Authorization' ? 'your-api-key' : undefined),
+    } as Request;
+
+    const context = {
+      getHandler: jest.fn(),
+      switchToHttp: () => ({
+        getRequest: () => request,
+      }),
+    } as unknown as ExecutionContext;
+
+    expect(apiKeyGuard.canActivate(context)).toBe(true);
+  });
+
+  it('should return false if the API key in the request header does not match the config', () => {
+    // Mocking the reflector to return false (not marked as public)
+    jest.spyOn(reflector, 'get').mockReturnValue(false);
+
+    // Mocking the ConfigService to return the expected API key
+    jest.spyOn(configService, 'get').mockReturnValue('your-api-key');
+
+    const request = {
+      header: (name: string) =>
+        name === 'Authorization' ? 'wrong-api-key' : undefined,
+    } as Request;
+
+    const context = {
+      getHandler: jest.fn(),
+      switchToHttp: () => ({
+        getRequest: () => request,
+      }),
+    } as unknown as ExecutionContext;
+
+    expect(apiKeyGuard.canActivate(context)).toBe(false);
   });
 });
